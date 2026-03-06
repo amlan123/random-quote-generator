2386a1221120b73720b1fc5a1b15b86c6360b58d
Amlan Kumar Pradhan <34065551+amlan123@users.noreply.github.com>
2026-03-06 13:57:03 +0100

    Create quotes.py with basic structure

---
+++ quotes.py
@@ -0,0 +1,13 @@
+import random
+
+quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Be yourself; everyone else is already taken. - Oscar Wilde",
+    "In the middle of difficulty lies opportunity. - Albert Einstein",
+    "Stay hungry, stay foolish. - Steve Jobs",
+    "The unexamined life is not worth living. - Socrates",
+    "Why do we fall? So we can learn to pick ourselves up. - Batman"
+]
+
+random_quote = random.choice(quotes)
+print(random_quote)
