57f991333121bc3591dcdcc93706f268e0ba0619
Amlan Kumar Pradhan <34065551+amlan123@users.noreply.github.com>
2026-03-06 14:00:29 +0100

    2nd

--- quotes.py
+++ quotes.py
@@ -7,6 +7,7 @@ quotes = [
     "Stay hungry, stay foolish. - Steve Jobs",
     "The unexamined life is not worth living. - Socrates",
     "Why do we fall? So we can learn to pick ourselves up. - Batman"
+    "Our life is a reflection of our actions. - Amlan"
 ]
 
 random_quote = random.choice(quotes)
